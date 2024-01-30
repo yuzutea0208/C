0e38f7f2f1422576454bc37e1272bd283910540e
youzichu <117759799+yuzutea0208@users.noreply.github.com>
2024-01-30 10:41:00 +0000

    对文件名等等进行整理

--- 第四章：字符串简介/4.4 printf()和scanf()/4.4.2 使用printf()/printout/printout.c
+++ 第四章：字符串简介/4.4 printf()和scanf()/4.4.2 使用printf()/printout/printout.c
@@ -9,7 +9,7 @@ int main(void)
 	float pies = 12.75;
 	int cost = 7800;
 
-	printf("有%d位参赛者吃了%f个草莓蛋糕。\n",numbber,cake);
+	printf("有%d位参赛者吃了%f个草莓派。\n",number,pies);
 	printf("圆周率的值是%f。\n",PI);
 	printf("你对我来说太贵咯。\n");
 	printf("%c%d\n",'$',2 * cost); /*这里$是一个字符char，会被c打印出来*/
